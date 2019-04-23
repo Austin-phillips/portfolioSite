056787e822b958bf3e6911f02a6e6824233352a2
Austin Phillips <phillips.austin51@gmail.com>
2019-04-22 18:10:59 -0600

    Fix server

--- server.js
+++ server.js
@@ -6,11 +6,7 @@ const port = process.env.SERVER_PORT
 
 const app = express();
 
-app.use(express.static('public'))
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
-})
+app.use(express.static(__dirname + '/public'))
 
 app.listen(port, () => console.log(`Serving on port ${port}`))
 
